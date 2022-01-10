21dcd4f5bb56902bb772a16fe19ca96b40264380
JY, Park <bleusoire@gmail.com>
2022-01-11 02:09:22 +0900

    applied spring security and polishing property files.

---
+++ src/main/java/dev/newv/discovery/config/PeerSecurityConfig.java
@@ -0,0 +1,22 @@
+package dev.newv.discovery.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Profile("peer1,peer2")
+public class PeerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/eureka/**").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+}
