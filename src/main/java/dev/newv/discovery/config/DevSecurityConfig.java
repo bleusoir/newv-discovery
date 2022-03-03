72738632d1abf9bbfe6081e94bd28dfcd08bb7bb
JY, Park <bleusoire@gmail.com>
2022-03-03 17:28:34 +0900

    added deployment manifest file for k8s.

--- src/main/java/dev/newv/discovery/config/DevSecurityConfig.java
+++ src/main/java/dev/newv/discovery/config/DevSecurityConfig.java
@@ -14,6 +14,7 @@ public class DevSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests().anyRequest().permitAll()
+                .and().headers().frameOptions().disable();
     }
 }
